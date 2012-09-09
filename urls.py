66f4e26d5b45d0312f3211e72dfd896c0a6607d5
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2012-09-09 10:40:35 +0200

    Add whois pages for the connecting ip and host

--- urls.py
+++ urls.py
@@ -2,4 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     url(r'^$', 'web.ip_info.views.main'),
+    url(r'^whois-ip/$', 'web.ip_info.views.whois_ip'),
+    url(r'^whois-host/$', 'web.ip_info.views.whois_host'),
 )
--- views.py
+++ views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render_to_response
 from pprint import pformat
 from socket import gethostbyaddr, herror
+from whelk import shell
+from publicsuffix import PublicSuffixList
+
+psl = PublicSuffixList()
 
 def main(request):
     ctx = {'ip': request.META['REMOTE_ADDR']}
@@ -13,3 +17,18 @@ def main(request):
     if 'HTTP_VIA' in request.META:
         ctx['proxy'] = request.META['HTTP_VIA']
     return render_to_response('ip_info/base.html', ctx)
+ 
+def whois_ip(request):
+    ctx = {'what': request.META['REMOTE_ADDR']}
+    ctx['data'] = shell.whois(request.META['REMOTE_ADDR']).stdout
+    return render_to_response('ip_info/whois.html', ctx)
+
+def whois_host(request):
+    ip = request.META['REMOTE_ADDR']
+    try:
+        ctx = {'what': psl.get_public_suffix(gethostbyaddr(request.META['REMOTE_ADDR'])[0])}
+    except herror:
+        ctx = {'what': '(hostname unavailable)', data: "Hostname not found"}
+    else:
+        ctx['data'] = shell.whois(ctx['what']).stdout
+    return render_to_response('ip_info/whois.html', ctx)
