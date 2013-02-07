686e1c1ced4d62fb69b5f9473e8494f00d79761e
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2013-02-07 14:04:24 +0100

    Display geo data, if available

--- views.py
+++ views.py
@@ -3,6 +3,11 @@ from pprint import pformat
 from socket import gethostbyaddr, herror
 from whelk import shell
 from publicsuffix import PublicSuffixList
+from django.conf import settings
+if hasattr(settings, 'GEOIP_DATA'):
+    import GeoIP
+else:
+    GeoIP is None
 
 psl = PublicSuffixList()
 
@@ -12,6 +17,13 @@ def main(request):
         ctx['hostname'] = gethostbyaddr(request.META['REMOTE_ADDR'])[0]
     except herror:
         pass
+    if GeoIP:
+        gi = GeoIP.open(settings.GEOIP_DATA, GeoIP.GEOIP_STANDARD)
+        geo = gi.record_by_addr(request.META['REMOTE_ADDR'])
+        for k in geo:
+            if isinstance(geo[k], str):
+                geo[k] = geo[k].decode('iso-8859-1', 'replace')
+        ctx['geo'] = geo
     if 'HTTP_X_FORWARDED_FOR' in request.META:
         ctx['local_ip'] = request.META['HTTP_X_FORWARDED_FOR']
     if 'HTTP_VIA' in request.META:
